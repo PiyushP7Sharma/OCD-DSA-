7a34dcb65cae7b6422c3ecef8b84d878e572494f
PiyushP7Sharma <piyushs0709@gmail.com>
2025-03-03 22:13:28 +0530

    Update and rename DAY 2 to DAY 2.cpp

--- DAY 2.cpp
+++ DAY 2.cpp
@@ -1,5 +1,5 @@
-DAY 2 
-Q1: IF THE STRING IS GOOD OR BAD
+//DAY 2 
+//Q1: IF THE STRING IS GOOD OR BAD
 
 class Solution {
   public:
@@ -28,7 +28,7 @@ class Solution {
     }
 };
 
-APPROACH:  chehcing the string for each index . If the string char is vowel then increase the vowel count by 1 and reset the consonat count to 0. If the string char is consonat then increase the consonant count by 1 and resent the vowel count. If the wildcard char comes then increase both vowel count and consonat count. meanwhile check the conditions for bad string 
+//APPROACH:  chehcing the string for each index . If the string char is vowel then increase the vowel count by 1 and reset the consonat count to 0. If the string char is consonat then increase the consonant count by 1 and resent the vowel count. If the wildcard char comes then increase both vowel count and consonat count. meanwhile check the conditions for bad string 
 
-TIME COMPLEXITY : O(N)
-SPAXE COMPLEXITY : O(1)
+//TIME COMPLEXITY : O(N)
+//SPAXE COMPLEXITY : O(1)
