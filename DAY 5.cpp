7ce96a2a57efff0ae18a509da0edd281e89be2e9
PiyushP7Sharma <piyushs0709@gmail.com>
2025-03-06 23:03:01 +0530

    Create DAY 5.cpp

---
+++ DAY 5.cpp
@@ -0,0 +1,25 @@
+//Q1 : Distribute Money to Maximum Children
+//TC:O(children) 
+//SC:O(1)
+class Solution {
+public:
+    int distMoney(int money, int children) {
+        if (money < children){
+            return -1;
+        }
+        money -= children;
+        int count = 0;
+        while (money >= 7 && children > 0) {
+            money -= 7;
+            count++;
+            children--;
+        }
+        if (children == 1 && money == 3) {
+            count--;
+        }
+        if (children == 0 && money > 0) {
+            return count - 1;
+        }
+        return count;
+    }
+};
