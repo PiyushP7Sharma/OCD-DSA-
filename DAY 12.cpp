c7cd0483239653ae410a278b0664592a84178524
PiyushP7Sharma <piyushs0709@gmail.com>
2025-03-13 18:14:12 +0530

    Update DAY 12.cpp

--- DAY 12.cpp
+++ DAY 12.cpp
@@ -15,3 +15,26 @@ public:
         return -1;
     }
 };
+
+//Q3:  search insert position 
+// Approach : Calculating the lower bound index 
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n= nums.size();
+        int low= 0;
+        int high = n-1;
+        int ans=n;
+        while (low<= high){
+            int mid =(low + high)/2;
+            if(nums[mid]>=target){
+                ans= mid;
+                high= mid-1;
+            }
+            else {
+                low= mid+1;
+            }
+        }
+        return ans;
+    }
+};
