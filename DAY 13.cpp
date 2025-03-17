9ee84ef171c9b4a7170bd0cee725db8f5e5a6fc3
PiyushP7Sharma <piyushs0709@gmail.com>
2025-03-17 22:09:46 +0530

    Create DAY 13.cpp

---
+++ DAY 13.cpp
@@ -0,0 +1,22 @@
+DAY 13
+Q2: FIND MIN IN ROTATED SORTED ARRAY
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] <= nums[right]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return nums[left];        
+    }
+};
+
